80976634238e10a61d302dce0a4d4ecc842bdfc6
И Марк <imark.work.ac@gmail.com>
2021-09-18 18:52:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./App.css";
+import StoreContext from "./StoreContext";
+
+const MyPostsContainer = () => (
+  <StoreContext.Consumer>
+    {(store) => (
+      <h1>
+        My Posts id: {store.id}, value: {store.name}
+      </h1>
+    )}
+  </StoreContext.Consumer>
+);
+
+const Profile = () => (
+  <div>
+    <MyPostsContainer />
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
+      <Profile />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import StoreContext from "./StoreContext";
+
+const store = {
+  id: 15,
+  name: "Store",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
