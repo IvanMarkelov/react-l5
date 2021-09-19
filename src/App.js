da5d91f396eeb788c28e4e6e52f0a4ec33b3ace4
И Марк <imark.work.ac@gmail.com>
2021-09-19 19:02:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import ForwardRefParentInput from "./ForwardRefParentInput";
 
 function App() {
   return (
     <div className="App">
+      <ForwardRefParentInput />
     </div>
   );
 }
