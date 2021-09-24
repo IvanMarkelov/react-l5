3d99256db2fec8faf8623e86fbb7e22c6378c78d
И Марк <imark.work.ac@gmail.com>
2021-09-24 18:08:32 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <a href="/profile">Profile</a>
+        <a href="/dialogs">Dialogs</a>
+      </nav>
+      <BrowserRouter>
+        <Route path="/dialogs" component={Dialogs} />
+        <Route path="/profile" component={Profile} />
+      </BrowserRouter>
     </div>
   );
+}
+
+function Dialogs(props) {
+  return <div>DIALOGS</div>;
+}
+
+function Profile(props) {
+  return <div>PROFILE</div>;
 }
 
 export default App;
