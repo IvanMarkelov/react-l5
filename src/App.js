705f023af1f6d500737460afa4ce590f10d8f919
И Марк <imark.work.ac@gmail.com>
2021-09-24 20:21:51 +0300

    i

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {
   BrowserRouter,
+  Switch,
+  Route,
   useLocation,
   useParams,
   useHistory,
@@ -9,23 +11,77 @@ import {
 import "./App.css";
 
 function App() {
-  const history = useHistory();
-
-  console.log(history);
-
-  const handleClick = () => {
-    history.push("/about");
-  };
-
   return (
     <div className="App">
       <BrowserRouter>
-        <h1>Some Page</h1>
-        <p>Some text</p>
-        <button onClick={handleClick}>To About</button>
+        <HeaderComponent />
+        <Switch>
+          <Route path="/about/:name" component={AboutComponent} />
+          <Route path="/" component={ChildComponent} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
 }
+
+const ChildComponent = () => {
+  const history = useHistory();
+
+  console.log(history);
+
+  const handleClick = (name) => {
+    history.push(`/about/${name}`);
+  };
+
+  const rich = "Rich Dad Poor Dad";
+  const richest = "The richest man in Babylon";
+
+  return (
+    <div>
+      <h1>Some Page</h1>
+      <p>Some text</p>
+      <button onClick={() => handleClick(rich)}>To {rich}</button>
+      <button onClick={() => handleClick(richest)}>To {richest}</button>
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  const location = useLocation();
+  const history = useHistory();
+  
+  console.log("location:");
+  console.log(location);
+
+  console.log("history:");
+  console.log(history);
+
+  const handleBackClick = () => {
+    history.goBack();
+  };
+
+  const atAboutPage = location.pathname.includes("/about");
+
+  return atAboutPage && <button onClick={handleBackClick}>Back</button>;
+};
+
+const AboutComponent = () => {
+  const { name } = useParams();
+  console.log("params");
+  console.log(useParams());
+
+  const match = useRouteMatch("/about/The richest man in Babylon");
+  console.log("match");
+  console.log(match);
+
+  const showBuyButton = match && match.isExact;
+
+  return (
+    <div>
+      <h1>About {name}</h1>
+      {showBuyButton && <button>Buy Now</button>}
+    </div>
+  );
+};
 
 export default App;
