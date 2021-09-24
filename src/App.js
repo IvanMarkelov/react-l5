d7e33c5f7d4ff72262b0369a51bf1585680c618b
И Марк <imark.work.ac@gmail.com>
2021-09-24 19:43:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {
+  BrowserRouter,
+  useLocation,
+  useParams,
+  useHistory,
+  useRouteMatch,
+} from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const history = useHistory();
+
+  console.log(history);
+
+  const handleClick = () => {
+    history.push("/about");
+  };
+
   return (
     <div className="App">
+      <BrowserRouter>
+        <h1>Some Page</h1>
+        <p>Some text</p>
+        <button onClick={handleClick}>To About</button>
+      </BrowserRouter>
     </div>
   );
 }
